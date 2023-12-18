5464f3bf05a0005e642f1f1c180c2bb26b35adee
Willd233 <emiliod255@gmail.com>
2023-12-17 22:23:16 -0400

    update set Iterset

--- src/app/component/TurnTimer/timer.tsx
+++ src/app/component/TurnTimer/timer.tsx
@@ -5,23 +5,22 @@ import { useEffect, useRef, useState } from "react"
 
 const Timer = () => {
     const [timer, setTimer] = useState(5);
-    const [isRunning, setIsRunning] = useState(false)
-    const timerRef= useRef();
 
-    
     const countDown = () => {
     setTimer((timer)=> Math.max(timer -1,0))
     };
     
-
-    useEffect(()=>{
+    const handleStartClick=()=>{
+        setTimer(5);
         const interval = setInterval(countDown, 1000);
         return ()=> clearInterval(interval);
-    },[]);
+    }
 
     return (
-    <span>{timer}</span>
-    
+        <div>    <span>{timer}</span>
+    <button onClick={handleStartClick}>start</button>
+    </div>
+
     )
 
 }
