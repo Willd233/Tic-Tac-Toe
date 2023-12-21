c8a034df23c5a3d092efd8012cfb093bf558be45
Willd233 <emiliod255@gmail.com>
2023-12-20 20:43:35 -0400

    #hh

--- src/app/component/Grid/Grid.tsx
+++ src/app/component/Grid/Grid.tsx
@@ -9,12 +9,14 @@ export const Grid = () => {
   const [turn, setTurn] = useState<"X" | "O">("X");
   const [playing, setPlaying] = useState<boolean>(false);
   const [textButton, setTextButton] = useState("PLAY");
-  const [winer, setWiner] = useState("Tic Tac Toe!")
+  const [winer, setWiner] = useState("Tic Tac Toe!");
+  const [winnerColor, setWinnerColor] = useState("");
+
 
 
   const handleClick = (i: number) => {
     if (!playing || cells[i]) return;
-    
+
 
     const newCells = [...cells];
     newCells[i] = turn;
@@ -33,7 +35,7 @@ export const Grid = () => {
       [0, 4, 8],
       [2, 4, 6],
     ];
-  
+
     for (const condition of winningConditions) {
       if (
         cells[condition[0]] &&
@@ -41,14 +43,14 @@ export const Grid = () => {
         cells[condition[1]] === cells[condition[2]]
       ) {
         setWiner(`${cells[condition[0]]} Wins!`);
+        setWinnerColor(cells[condition[0]] === "X" ? "#940a0a" : "orange"); // Set the color here
         setPlaying(false);
         setTextButton("PLAY");
-        setCells(Array(9).fill(""));
         setTurn("X");
         break;
       }
     }
-    
+
     if (cells.every((cell) => cell)) {
       setWiner("It's a Tie!");
       setPlaying(false);
@@ -56,25 +58,29 @@ export const Grid = () => {
       setTurn("X");
       setTextButton("PLAY");
     }
+
+    
   }, [cells]);
-  
+
 
   const startTimer = () => {
     setPlaying(true)
     setTextButton("IN PROGRESS")
+    setCells(Array(9).fill(""));
+
   }
 
   return (
     <main>
       <div className='primary-title'>
-        <h1>{winer}</h1>
+      <h1 style={{ color: winnerColor }}>{winer}</h1>
       </div>
       <div className="container">
-        <div className={`X ${turn === "X" && playing ? "turn-active" : ""}`}>
+        <div className={`X ${turn === "X" && playing ? "turn-active" : ""} `}>
           <div className="title">
             <h1>X Turn</h1>
           </div>
-          <div className="timer-left O">
+          <div className="timer-left O" >
             <span className="title-internal">
               <Timer playing={playing} turn={turn} setTurn={setTurn} tag={"O"} />
             </span>
@@ -87,9 +93,9 @@ export const Grid = () => {
               className={`Icon ${cell ? "filled" : ""} ${turn === "X" ? "hover-X" : "hover-O"}`}
               data-cell={cell}
               onClick={() => handleClick(i)}
-              
+
             >
-              {cell === "X" && <FontAwesomeIcon icon={faXmark} className="icon-red" />}
+              {cell === "X" && <FontAwesomeIcon icon={faXmark} className="icon-red"  style={{ color: winnerColor }}/>}
               {cell === "O" && <FontAwesomeIcon icon={faO} className="icon-orange" />}
             </span>
           ))}
@@ -106,7 +112,7 @@ export const Grid = () => {
         </div>
         <button
           onClick={startTimer}
-          
+
           className={`${playing ? "in-progress" : "play"}`}
         >
           {textButton}
